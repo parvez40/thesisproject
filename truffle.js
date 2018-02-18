121fee0a8f261890eb10ee9257f1b924cd2fa1a7
pahmedxain <parvez.ahmed@xain-group.com>
2018-02-18 19:58:11 +0100

    fifth commit

--- truffle.js
+++ truffle.js
@@ -4,6 +4,20 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+      rinkeby: {
+          host: "localhost",
+          port: 8545,
+          network_id: "4", // Match any network id
+          //If we want to use another account except
+          // main etherbase account(first account) to deploy then it's address
+          // should be mentioned to from, if account address is not mentioned
+          //then it will use the first account or main account automatically to deploy
+          //the contract
+          from: "0x55d5c7b61be5873c70dfd36102ce2244fb1b8333",
+          //this is the gas limit that will used in time of deployment
+          gas: 5600000
+      }
+
   }
 };
